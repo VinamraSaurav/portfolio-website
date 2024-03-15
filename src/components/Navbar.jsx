e5759f6d727ba56b4de875d8dddf2d73265365dd
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-03-15 18:28:38 +0530

    footer issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { FaGraduationCap } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='m-0'>
     <div className='max-w-7xl mx-auto fixed top-0 right-0 left-0 hidden xls:block backdrop-blur-[21px] z-50'>
         <div className='bg-color-5 h-10 xls:h-14 my-6 mx-3 flex items-center text-color-1 justify-between p-4 rounded-lg'>
             <div className='text-lg xls:text-xl font-popins font-[1000] stroke-2 stroke-black '>
@@ -24,7 +24,7 @@ const Navbar = () => {
         </div>
 
     </div>
-    <div className='fixed bottom-0 left-0 right-0 xls:hidden flex bg-white h-12 justify-around rounded-t-md'>
+    <div className='fixed bottom-[0] left-0 right-0 xls:hidden flex bg-white h-12 justify-around rounded-t-md'>
     <div className='flex gap-2 items-center hover:text-color-5 hover:bg-color-1 py-1 px-3 rounded-b-2xl hover:cursor-pointer'><MdHome className='text-3xl'/> </div>
                 <div className='flex gap-2 items-center hover:text-color-5 hover:bg-color-1 py-1 px-3 rounded-b-2xl hover:cursor-pointer'><GoProjectSymlink className='text-3xl'/></div>
                 <div className='flex gap-2 items-center hover:text-color-5 hover:bg-color-1 py-1 px-3 rounded-b-2xl hover:cursor-pointer'><MdOutlineWork className='text-3xl'/></div>
