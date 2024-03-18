b1a7ec10fde6ee034ed79629359e67c30a0a0b70
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-03-18 11:00:53 +0530

    completed portfolio website

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -13,8 +13,7 @@ const ProjectCard = ({data}) => {
             </div>
             <div>
                 <div className='text-white text-lg font-bold font-popins mt-3 mb-[7px] line-clamp-2 max-h-[53px]- '>{title} </div>
-                <div className='text-white text-sm font-popins mb-3 h-[120px] line-clamp-6'>Foodigy is a food delivering website. It uses Swiggy API to fetch data. It offers config drivern UI, Shimmer UI feature.
-It is build on React.js. Lorem ipsum dolor.  It uses Swiggy API to fetch data. It offers config drivern UI, Shimmer UI feature.</div>
+                <div className='text-white text-sm font-popins mb-3 h-[120px] line-clamp-6'>{description}</div>
             <div className='absolute bottom-3 left-2 right-2 z-0'>
             <div className='flex justify-around'>
                 <Link href={liveLink} target='_blank'><div className='flex gap-2 px-3 py-1 rounded-md bg-color-2 text-white font-semibold font-popins items-center text-md hover:cursor-pointer'>LIVE<FaExternalLinkAlt className='text-sm font-thin' /></div></Link>
--- src/constants/projectData.js
+++ src/constants/projectData.js
@@ -6,7 +6,7 @@ const projectData =[
         'image' : '/projectImages/foodigy.png',
         'repoLink':'https://github.com/VinamraSaurav/food-delivering-webApp-frontend',
         'liveLink':'https://foodigy.netlify.app/',
-        'description' : '',
+        'description' : 'Foodigy is a React-based website  for a food delivery company. This website features a configuration-driven user interface, powered by the Swiggy API for fetching restaurant and menu data. Notable features include a shimmer UI for an enhanced user experience, a search function, contact forms, and a single-page application structure. The project utilizes technologies such as React, HTML, CSS, JavaScript, and Tailwind CSS. ',
 
     },
     {
@@ -15,7 +15,7 @@ const projectData =[
         'image' : '/projectImages/algoarena.png',
         'repoLink':'https://github.com/MLSAKIIT/AlgoArena',
         'liveLink':'https://algo-arena-nine.vercel.app/',
-        'description' : '',
+        'description' : 'AlgoArean is a group project, it is a dynamic website built on Next.js, empowers users—whether developers or learners—with an extensive roadmap for their developer journey. Beyond roadmaps, AlgoArena offers features like roadmap forking, personalized customization, login/signup, and community interaction. As a Project Partner, my key contributions included significant frontend development, designing the captivating hero section, building the complete learning page, implementing sliders, creating the Learning Path Accordion, and developing the saved courses page. This exciting journey provided valuable insights into team collaboration, version control, routing in Next.js, and both server-side and client-side rendering. I take pride in contributing to AlgoArena’s growth and impact!',
 
     },
     {
@@ -24,7 +24,7 @@ const projectData =[
         'image' : '/projectImages/compwallah.png',
         'repoLink':'https://github.com/VinamraSaurav/compwallah',
         'liveLink':'https://compwallah.vercel.app/',
-        'description' : '',
+        'description' : 'The CompWallah website is an individual project. It features a sleek and modern design, meticulously crafted using Next.js, Tailwind CSS, JavaScript, and HTML. Key dependencies such as Formik, Yup, React Slick, Formspree, and react-hot-toast enhance functionality and user interactions. Despite challenges like cross-browser compatibility and responsiveness optimization, the project delivers a seamless user experience',
 
     },
     {
@@ -33,7 +33,7 @@ const projectData =[
         'image' : '/projectImages/fed-kiit.png',
         'repoLink':'https://github.com/VinamraSaurav/fed-kiit-website-project',
         'liveLink':'https://fed-kiit-vin.netlify.app/',
-        'description' : '',
+        'description' : 'The FED-KIIT-Website is a React-based web app developed during the recruitment process for FED KIIT, a society at KIIT University. It showcases my proficiency in cutting-edge web development technologies, emphasizing user-centric solutions. Key accomplishments include designing and developing a visually stunning and responsive website using HTML5, CSS, and React.js, incorporating Tailwind CSS for dynamic interactions, and implementing a carousel with an autoplay feature.',
 
     },
     {
@@ -42,7 +42,7 @@ const projectData =[
         'image' : '/projectImages/portfolio-vin.png',
         'repoLink':'https://github.com/VinamraSaurav/portfolio-website',
         'liveLink':'https://vinamra-saurav.vercel.app/',
-        'description' : '',
+        'description' : 'You are viewing my Portfolio Website. 😊',
 
     }
 ]
